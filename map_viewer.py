861cd8423ab8c133e5bc1b2755bb0e109eee4c9a
Adrian <andarms@outlook.com>
2015-10-12 23:35:12 -0500

    Add Distance label
    Calculate the distance in pixel applying distance between two points

--- map_viewer.py
+++ map_viewer.py
@@ -1,8 +1,9 @@
 import pygame as pg
 import prepare
 
-from state_engine import GameState
+from labels import Label
 from line import Line
+from state_engine import GameState
 
 
 class MapViewer(GameState):
@@ -20,7 +21,14 @@ class MapViewer(GameState):
         self.map = prepare.GFX[self.map_name.replace(" ", "-")]
         self.rect = self.map.get_rect(center=self.screen_rect.center)
         self.map_scale = prepare.SCALES[self.map_name]
+
         self.line = None
+        self.distance = 0.0
+
+        self.font = prepare.FONTS["Saniretro"]
+        self.distance_label = None
+        self.text_color = (255, 255, 255)
+        self.bg_color = (0, 0, 0)
 
     def get_event(self, event):
         if event.type == pg.QUIT:
@@ -28,7 +36,7 @@ class MapViewer(GameState):
 
         if event.type == pg.MOUSEBUTTONUP:
             if self.line and self.line.moving:
-                self.line.set_end(event.pos)
+                self.set_anchor_point(event.pos)
             else:
                 self.create_new_line(event.pos)
 
@@ -44,6 +52,18 @@ class MapViewer(GameState):
         surface.blit(self.map, self.rect)
         if self.line:
             self.line.draw(surface)
+            self.distance_label.draw(surface)
 
     def create_new_line(self, pos):
         self.line = Line(pos)
+        self.distance = 0.0
+        text = "{:.2f} miles".format(self.distance)
+        self.distance_label = Label(self.font, 20, text,
+                                    self.text_color, {"topleft": (0, 0)},
+                                    self.bg_color)
+
+    def set_anchor_point(self, pos):
+        self.line.set_end(pos)
+        self.distance = self.line.distance * self.map_scale
+        text = "{:.2f} miles".format(self.distance)
+        self.distance_label.set_text(text)
--- prepare.py
+++ prepare.py
@@ -3,7 +3,7 @@ import pygame as pg
 import tools
 
 
-SCREEN_SIZE = (650, 660)
+SCREEN_SIZE = (650, 680)
 ORIGINAL_CAPTION = "Map Viewer"
 
 pg.init()
@@ -12,13 +12,15 @@ pg.display.set_caption(ORIGINAL_CAPTION)
 SCREEN = pg.display.set_mode(SCREEN_SIZE)
 SCREEN_RECT = SCREEN.get_rect()
 
-#Scale factors for the different maps
+# Scale factors for the different maps
 SCALES = {
-        "North America": 600. / 68,
-        "South America": 500. / 60,
-        "Africa": 800. / 74,
-        "Europe": 300. / 65,
-        "Oceania": 600. / 45,
-        "Asia": 800. / 76}
-        
+    "North America": 600. / 68,
+    "South America": 500. / 60,
+    "Africa": 800. / 74,
+    "Europe": 300. / 65,
+    "Oceania": 600. / 45,
+    "Asia": 800. / 76
+}
+
 GFX = tools.load_all_gfx(os.path.join("resources", "graphics"))
+FONTS = tools.load_all_fonts(os.path.join("resources", "fonts"))
